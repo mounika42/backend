0c7e221e2726c4bc69b399c4f7c6456c8a13263e
admin <admin@hp>
2018-01-11 02:37:11 -0500

    BACKEND

---
+++ src/main/java/com/spring/dao/CategoryDAO.java
@@ -0,0 +1,11 @@
+package com.spring.dao;
+
+import com.spring.model.*;
+
+public interface CategoryDAO 
+{
+	public void insertCategory(Category category);
+
+	
+
+}
---
+++ src/main/java/com/spring/dao/UserDAO.java
@@ -0,0 +1,12 @@
+package com.spring.dao;
+
+import com.spring.model.User;
+
+public interface UserDAO
+{
+
+		public void insertUser(User user);
+
+
+
+}
